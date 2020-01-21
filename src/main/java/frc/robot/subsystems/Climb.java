fc46780f292a0b1769dadce9efff1971a526d50a
Mike Lydon <PragmaticMike@gmail.com>
2020-01-20 17:22:08 -0700

    Add two new set() functions to match the WPI_TalonSRX functions.

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -4,6 +4,7 @@ import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,5 +17,13 @@ public class Climb extends SubsystemBase {
 
     public void set( ControlMode control_mode, double magnitude) {
         climb_talon.set(control_mode, magnitude);
+    }
+
+    public void set(double speed) {
+        climb_talon.set(speed);
+    }
+
+    public void set(ControlMode mode, double demand0, DemandType demand1Type, double demand1) {
+        climb_talon.set(mode, demand0, demand1Type, demand1);
     }
 }
