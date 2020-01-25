ac051e9f912bfdeeb002dca162d526513958e1a4
Triston-Van-Wyk <>
2020-01-25 14:01:37 -0700

    Add the Java Documintation Comments to the commands and subsyestems

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -29,12 +29,10 @@ public class JoystickDrive extends CommandBase {
     addRequirements(m_drivetrain);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     double y = -joyY.getAsDouble(); // Because Negative is forward on the joysticks
@@ -44,12 +42,10 @@ public class JoystickDrive extends CommandBase {
     m_drivetrain.diffDrive(y, z, x);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -15,14 +15,27 @@ public class Climb extends SubsystemBase {
         climb_talon = new WPI_TalonSRX(Constants.CLIMB_TALON);
     }
 
+    /** This tells the code to set the climb talon's contol mode and magnitude
+     * @param control_mode This is the controm mode of the motor
+     * @param magnitude This the magnitude of the motor
+     */
     public void set( ControlMode control_mode, double magnitude) {
         climb_talon.set(control_mode, magnitude);
     }
 
+    /** This tells the motor what set it's speed to
+     * @param speed This is the speed of the motor
+     */
     public void set(double speed) {
         climb_talon.set(speed);
     }
 
+    /** This sets the mode, demand and demand type for the climb motors
+     * @param mode This is the mode of the control mode
+     * @param demand0 This is the demand 1 
+     * @param demand1Type This is the type of demand for demand 2
+     * @param demand1 This is demand 2
+     */
     public void set(ControlMode mode, double demand0, DemandType demand1Type, double demand1) {
         climb_talon.set(mode, demand0, demand1Type, demand1);
     }
