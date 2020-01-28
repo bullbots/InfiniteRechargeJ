737d961bdb3fd32d94a2f759e42eff0fd7ff7adb
Devin Vander Stelt <devin@vstelt.com>
2020-01-27 18:40:15 -0800

    Add organization to constant.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,32 @@
 package frc.robot;
 
 public final class Constants {
+
+    /**
+     * ALL PORTS GO HERE
+     */
+
+     // Drivetrain Ports
     public static int LEFT_MASTER_PORT = 1;
     public static int LEFT_SLAVE_PORT = 2;
     public static int RIGHT_MASTER_PORT = 3;
     public static int RIGHT_SLAVE_PORT = 4;
 
+    // Intake Ports
+    public static int INTAKE_PORT = 5;
+    public static int INTAKE_Wheel_PORT = 8;
+
+    // Shooter Ports
+    public static int TOP_SHOOTER_PORT = 6;
+    public static int BOTTOM_SHOOTER_PORT = 7;
+
+    // Climb Ports
     public static int CLIMB_TALON = 6;
+
+
+    /**
+     * ALL DRIVETRAIN RELATED CONSTANTS GO HERE
+     */
 
     public static double LEFT_MASTER_FF = 0.2;
     public static double LEFT_MASTER_P = 0.2;
@@ -24,20 +44,34 @@ public final class Constants {
     public static int RIGHT_MASTER_ACCELERATION = 3300;
     public static int RIGHT_MASTER_VELOCITY = 3300;
 
-    public static int kTIMEOUT_MS = 0;
+    public static double DRIVETRAIN_RAMP = 0.5;
 
-    public static int TOP_SHOOTER_PORT = 6;
-    public static int BOTTOM_SHOOTER_PORT = 7;
+
+    /**
+     * ALL INTAKE RELATED CONSTANTS GO HERE
+     */
+
+
+    /**
+     * ALL SHOOTER RELATED CONSTANTS GO HERE
+     */
 
     public static double SHOOTER_FF = 0;
     public static double SHOOTER_P = 5e-5;
     public static double SHOOTER_I = 1e-6;
     public static double SHOOTER_D = 0;
-  
-    // Neo motor contants
-    public static double NEO_MAX_RPM = 5676; // http://www.revrobotics.com/rev-21-1650/ "Empirical Free Speed"
-  
-    public static int INTAKE_PORT = 5;
-    public static double DRIVETRAIN_RAMP = 0.5;
-    public static int INTAKE_Wheel_PORT = 8;
+
+    public static double NEO_MAX_RPM = 5676; // http://www.revrobotics.com/rev-21-1650/ "Empirical Free Speed"    
+
+
+    /**
+     * ALL CLIMB RELATED CONSTANTS GO HERE
+     */
+
+
+     /**
+      * MISCELLANEOUS CONSTANTS
+      */
+
+    public static int kTIMEOUT_MS = 0;
 }
