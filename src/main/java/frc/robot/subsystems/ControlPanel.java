fa3fa5beee350d825d360e1c01fe348e095f720c
Triston-Van-Wyk <>
2020-03-02 20:00:16 -0700

    I added the commands
    I added the control panel commands. I am not done I can continue to work on it.

---
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.util.SafeTalonFX;
+import frc.robot.util.SafeTalonSRX;
+
+public class ControlPanel extends SubsystemBase {
+
+  private SafeTalonSRX control_motor;
+
+  private NetworkTableEntry controlVelocity;
+
+  public ControlPanel() {
+    configureShuffleBoard();
+
+    control_motor = new SafeTalonSRX(Constants.CONTROL_PORT);
+  }
+
+  /** This sets the intake motor
+   * @param val This is a Value that sets the motor
+   */
+  public void setcontrol(double val){
+    control_motor.set(val);
+  }
+
+  private void configureShuffleBoard() {
+    controlVelocity = Shuffleboard.getTab("Control")
+                  .add("Intake Encoder Velocity", 0)
+                  .withSize(2, 2)
+                  .withSize(0, 2)
+                  .withWidget(BuiltInWidgets.kGraph)
+                  .getEntry();
+  }
+  @Override
+  public void periodic() {
+  }
+
+  public void stop(){
+    control_motor.stopMotor();
+  }
+}
