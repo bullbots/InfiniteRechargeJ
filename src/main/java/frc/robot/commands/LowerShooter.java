3bf14064e5af6fefbe752236ea5b2d12558ebf20
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2020-03-02 19:47:05 -0700

    Deleted and commented out things

--- src/main/java/frc/robot/commands/LowerShooter.java
+++ src/main/java/frc/robot/commands/LowerShooter.java
@@ -31,8 +31,6 @@ public class LowerShooter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    System.out.println("LowerShooter is running");
-
     shooter.raiseSolenoid();
   }
 
